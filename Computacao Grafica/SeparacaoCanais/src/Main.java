8c1ca10c78e49e7dca7c249a724fc865ee43ebfd
Evandro Brunassi <ebrunassi@hotmail.com>
2019-04-16 20:25:08 -0300

    Separacao de Canais
    Dada uma imagem, o programa a separa em 3 tonalidades de cinza.
    A imagem exemplo está dentro da pasta src.

---
+++ Computacao Grafica/SeparacaoCanais/src/Main.java
@@ -0,0 +1,54 @@
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
+
+public class Main {
+
+	public static int calculaMedia(double[] rgb) {
+		double media = (rgb[0] + rgb[1] + rgb[2])/3;
+		return (int) media;
+	}
+	public static Mat imCinza(Mat im) {
+		Mat imC = im.clone();
+		for(int i = 0 ; i < im.height(); i++) {
+			for(int j = 0; j < im.width(); j++) {
+				double []rgb = im.get(i, j);
+				int media = calculaMedia(rgb);
+				rgb[0] = media;
+				rgb[1] = media;
+				rgb[2] = media;
+				imC.put(i,j, rgb);
+			}
+		}
+		return imC;
+	}
+	public static void separaCanais(Mat im) {
+		Mat imR = new Mat(im.rows() , im.cols() , CvType.CV_8UC1);
+		Mat imG = new Mat(im.rows() , im.cols() , CvType.CV_8UC1);
+		Mat imB = new Mat(im.rows() , im.cols() , CvType.CV_8UC1);
+//		imR = imCinza(im);
+		for(int i = 0 ; i < im.height(); i++) {
+			for(int j = 0; j < im.width(); j++) {
+				double []rgb = im.get(i, j);
+//				int mediaR = calculaMedia(rgb);				
+				
+				imB.put(i,j, rgb[0]);
+				imG.put(i,j, rgb[1]);
+				imR.put(i,j, rgb[2]);
+				
+			}
+		}
+		Imgcodecs.imwrite("C:\\Users\\ebrun\\Documents\\lady_blue.jpg", imB);
+		Imgcodecs.imwrite("C:\\Users\\ebrun\\Documents\\lady_green.jpg", imG);
+		Imgcodecs.imwrite("C:\\Users\\ebrun\\Documents\\lady_red.jpg", imR);
+	}
+	public static void main(String[] args) {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		Mat im = Imgcodecs.imread("C:\\Users\\ebrun\\Documents\\lady.jpg");
+
+		separaCanais(im);
+		System.out.println("Feito!");
+	}
+
+}
